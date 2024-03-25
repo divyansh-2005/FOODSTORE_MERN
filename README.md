93595a94bc4f936a2d17d98d81a4ba4660a64243
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-03-26 00:27:44 +0530

    Bcrypt.js in React,password Hashing

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
     `npm init`
     `npm install express nodemon mongoose`
     cmd to run `nodemon index.js`
+
+    
+    `npm install bcryptjs`
+    `npm install jsonwebtoken`
--- mernapp/backend/Routes/Createuser.js
+++ mernapp/backend/Routes/Createuser.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require("bcrypt");
 
 router.post("/createuser",
     body('email').isEmail(),
@@ -14,9 +15,12 @@ router.post("/createuser",
         }
 
         try {
+            const salt = await bcrypt.genSalt(10);
+            const securePassword = await bcrypt.hash(req.body.password, salt);
+
             await User.create({
                 name: req.body.name,
-                password: req.body.password,
+                password: securePassword,
                 email: req.body.email,
                 location: req.body.location,
             });
@@ -32,14 +36,15 @@ router.post("/loginuser",
     body('email').isEmail(),
     body('password', 'Incorrect Password').isLength({ min: 5 }),
     async (req, res) => {
-        let email = req.body.email;
+        const email = req.body.email;
         try {
-            let userData = await User.findOne({ email });
+            const userData = await User.findOne({ email });
             if (!userData) {
                 return res.status(400).json({ errors: "Try logging in with correct credentials" });
             }
 
-            if (req.body.password !== userData.password) {
+            const passwordMatch = await bcrypt.compare(req.body.password, userData.password);
+            if (!passwordMatch) {
                 return res.status(400).json({ errors: "Try logging in with correct credentials" });
             }
 
