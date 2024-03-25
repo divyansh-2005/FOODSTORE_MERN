5232558f27cb5f69deba3a7724145917ab8e5bf3
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-03-25 23:45:00 +0530

    backend-fronted link Sign Up & Login page, Express Validator

--- mernapp/src/App.jsx
+++ mernapp/src/App.jsx
@@ -9,6 +9,7 @@ import{
 import Login from './screens/Login';
 import 'bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Signup from './screens/Signup';
 
 
 function App() {
@@ -19,6 +20,8 @@ function App() {
       <Routes>
         <Route exact path ="/" element={<Home/>} />
         <Route exact path='/login' element={<Login/>} />
+        <Route exact path='/createuser' element={<Signup/>} />
+
       </Routes>
      </div>
     </Router>
