aa3c7ba191b3d74c9c732fd40de0fb2de73c1bb1
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-03-25 11:39:31 +0530

    project stared

---
+++ README.md
@@ -0,0 +1,2 @@
+1. install react
+    `npx create-react-app `
---
+++ mernapp/src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navebar from '../components/Navebar'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+        <div><Navebar/></div>
+        <div>Body</div>
+        <div><Footer/></div>
+    </div>
+  )
+}
