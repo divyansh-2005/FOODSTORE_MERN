0cb206e4d3a7c0633293413cdef5bd51edf36d78
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-03-25 13:52:40 +0530

    created componts

--- mernapp/src/App.jsx
+++ mernapp/src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css';
 import Home from './screens/Home';
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
+import Login from './screens/Login';
+import 'bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 function App() {
   return (
   <>
-    <div>
-      <Home/>
-    </div>
+    <Router>
+     <div>
+      <Routes>
+        <Route exact path ="/" element={<Home/>} />
+        <Route exact path='/login' element={<Login/>} />
+      </Routes>
+     </div>
+    </Router>
   </>
   );
 }
--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import Navebar from '../components/Navebar'
-import Footer from '../components/Footer'
+import React from "react";
+import Navebar from "../components/Navebar";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+import Carousal from "../components/Carousal";
 
 export default function Home() {
   return (
     <div>
-        <div><Navebar/></div>
-        <div>Body</div>
-        <div><Footer/></div>
+      <div> <Navebar /> </div>
+      <div> <Carousal /> </div>
+      <div className="m-3"> <Card /> </div>
+      <div> <Footer /> </div>
     </div>
-  )
+  );
 }
